429929d671b9bc0cad1e4827279ad4950c5b870c
denysmarz <denysdramz@gmail.com>
2021-09-01 18:02:17 -0400

    nuevo

--- clase/tarea-tema-4/parimpar.js
+++
@@ -1,5 +0,0 @@
-function parimpar(n){
-    if(n%2==0){
-        document.writeln("par");
-    }else document.writeln("impar");
-}
--- primer-examen/bdconx.php
+++
@@ -1,5 +0,0 @@
-<?php
-$conex = new mysqli("localhost","root","","bd_banco");
-if ($conex->connect_error)
- die ("conexion fallada".$conex->connect_error);
-?>
